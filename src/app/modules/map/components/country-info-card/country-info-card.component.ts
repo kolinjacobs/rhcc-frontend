54045c074523982351eb3c09d72c438dee51878e
Evan Jenkins <evan@activelamp.com>
2018-09-09 10:42:55 -0700

    fixing transition speed

--- src/app/modules/map/components/country-info-card/country-info-card.component.ts
+++ src/app/modules/map/components/country-info-card/country-info-card.component.ts
@@ -37,7 +37,7 @@ import {
       transition('void => *', [
         animate('.5s', keyframes([
           style({opacity: 0, offset: 0}),
-          style({opacity: 0, offset: 0.6}),
+          style({opacity: 0, offset: 0.8}),
           style({opacity: 1, offset: 1.0})
         ]))
       ])
