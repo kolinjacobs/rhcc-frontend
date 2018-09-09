b801468d276bcf49d06dcc95d2edd8376a6ab255
Evan Jenkins <evan@activelamp.com>
2018-09-08 22:57:00 -0700

    adding some simple animations, might remove

--- src/app/modules/map/containers/map/map.component.ts
+++ src/app/modules/map/containers/map/map.component.ts
@@ -94,6 +94,7 @@ export class MapComponent implements AfterContentInit {
       .attr('r', (d) => Math.max(10 , Math.min(d.count * 15, 80)) + 'px')
       .attr('fill', '#EBA313')
       .attr('name', (d) => d.name)
+      .attr('class', 'outer-circle')
       .attr('type', 'mission')
       .attr('opacity', 0.15);
 
