5223d9bb2acf47829eb8fdb2235aca9c1c882ff2
Evan Jenkins <evan@activelamp.com>
2018-09-12 23:17:43 -0700

    adding some animations

--- src/app/modules/map/components/country-details/country-details.component.ts
+++ src/app/modules/map/components/country-details/country-details.component.ts
@@ -1,10 +1,29 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Country } from '../../models/country.model';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition, keyframes, query, stagger
+} from '@angular/animations';
 @Component({
   selector: 'rh-country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.scss']
+  styleUrls: ['./country-details.component.scss'],
+  animations: [
+    // trigger('listAnimation', [
+    //   transition('* => *', [
+    //     query(':enter', style({ opacity: 0 }), {optional: true}),
+    //     query(':enter', stagger('100ms', [
+    //       animate('.3s ease-in', keyframes([
+    //         style({opacity: 0, transform: 'translateY(75%)', offset: 0}),
+    //         style({opacity: .5, transform: 'translateY(-25px)',  offset: 0.3}),
+    //         style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+    //       ]))]), {optional: false})
+    //   ])
+    // ])
+  ]
 })
 export class CountryDetailsComponent implements OnInit {
   @Input() country: Country;
--- src/app/modules/map/components/mission-details/mission-details.component.ts
+++ src/app/modules/map/components/mission-details/mission-details.component.ts
@@ -1,9 +1,45 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition, keyframes
+} from '@angular/animations';
 
 @Component({
   selector: 'rh-mission-details',
   templateUrl: './mission-details.component.html',
-  styleUrls: ['./mission-details.component.scss']
+  styleUrls: ['./mission-details.component.scss'],
+  animations: [
+    trigger('appearIn', [
+      state('in', style({ opacity: 1 })),
+      transition('void => *', [
+        animate('.5s', keyframes([
+          style({opacity: 0, offset: 0}),
+          style({opacity: 0, offset: 0.8}),
+          style({opacity: 1, offset: 1.0})
+        ]))
+      ])
+    ]),
+    trigger('slideInRight', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        animate(400, keyframes([
+          style({opacity: 0, transform: 'translateX(100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(-15px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        ]))
+      ]),
+      transition('* => void', [
+        animate(400, keyframes([
+          style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+          style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+          style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+        ]))
+      ])
+    ]),
+  ]
 })
 export class MissionDetailsComponent implements OnInit {
   @Input() selectedMission: any;
